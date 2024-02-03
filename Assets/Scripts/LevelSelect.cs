25a7058cedf1b9d891da1676f73a818dea5ecae8
Grant Brown <onlythebest@gmail.com>
2024-02-02 18:04:26 -0800

    working level select
    bam!

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Load selected level on button press
+    public void LoadLevel()
+    {
+        //string selectedLevel = EventSystem.current.currentSelectedGameObject.name;
+        //SceneManager.LoadScene(selectedLevel);
+        SceneManager.LoadScene("0.5");
+    }
+}
