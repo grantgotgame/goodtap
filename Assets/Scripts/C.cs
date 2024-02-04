175dbbb4b03457ef8b08f8e49d0877072908b2db
Grant Brown <onlythebest@gmail.com>
2024-02-04 15:46:16 -0800

    copy B to C

---
+++ Assets/Scripts/C.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class C : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip good;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Do something on button press
+    public void Button()
+    {
+        //Play a sound
+        audioSource.PlayOneShot(good);
+        
+        // Mark level as complete
+        PlayerPrefs.SetInt("B", 1);
+
+        StartCoroutine(WaitLoad());
+    }
+
+    // Wait, then load Level Select
+    IEnumerator WaitLoad()
+    {
+        //Wait 1 second
+        yield return new WaitForSeconds(1);
+        
+        // Load level select
+        SceneManager.LoadScene("LevelSelect");
+    }
+}
